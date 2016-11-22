c1d0e1800eb7038f6f21e2fecbeb3e19a93c8529
Guillaume Simonneau <milu@localhost.localdomain>
2016-11-22 09:14:34 +0100

    lean config file

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -m
 /run/miscellaneous/restore_config.sh
-
+/run/miscellaneous/edit_config.sh
 $@ &
 
 fg
--- src/miscellaneous/restore_config.sh
+++ src/miscellaneous/restore_config.sh
@@ -3,5 +3,4 @@ mkdir -p /etc/httpbeat
 
 if [ ! -f /etc/httpbeat/httpbeat.yml ]; then
     cp /.backup/httpbeat/httpbeat.yml /etc/httpbeat/httpbeat.yml
-    /run/miscellaneous/edit_config.sh
 fi
